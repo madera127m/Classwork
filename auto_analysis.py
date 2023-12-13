225113da4ec7049fe9fc00cbbbbe18c389a7cea3
madera127m <143347046+madera127m@users.noreply.github.com>
2023-12-12 22:39:01 -0500

    Add files via upload

---
+++ auto_analysis.py
@@ -0,0 +1,66 @@
+
+
+
+
+
+import pandas as pd
+from matplotlib import pyplot as plt
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+
+
+
+df=pd.read_csv("auto-mpg.csv")
+
+df.head()
+
+y=np.array(df["displacement"]).reshape(398,1)
+
+np.shape(y)
+
+X=np.array(df["acceleration"]).reshape(398,1)
+
+np.shape(X)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
+
+fig, ax = plt.subplots()
+ax.scatter(X_train,y_train)
+ax.set_xlabel('Acceleration')
+ax.set_ylabel('Displacement')
+fig.savefig('xtrain_accel_vs_ytrain_disp.png')
+
+lr=LinearRegression()
+lr.fit(X_train,y_train)
+
+minX=min(X_test)
+maxX=max(X_test)
+
+[miny, maxy] = lr.predict(np.array([minX,maxX]).reshape(-1,1))
+
+miny,maxy
+
+fig, ax = plt.subplots()
+ax.plot([minX, maxX], [miny, maxy])
+ax.set_xlabel('Acceleration')
+ax.set_ylabel('Displacement')
+fig.savefig('linear_regression.png')
+
+r_squared = lr.score(X_test, y_test)
+r_squared
+
+with open('scriptnotes.txt', 'w') as report_file:
+        report_file.write("Dataset Info:\n")
+        report_file.write(str(df.info()) + '\n\n')
+
+        report_file.write("Summary Statistics:\n")
+        report_file.write(str(df.describe()) + '\n\n')
+
+        r_squared = lr.score(X_test, y_test)
+        report_file.write(f"R-squared score from lr.score of the test data: {r_squared:.4f}\n")
+        report_file.write("The graphs show a negative correlation between displacement and acceleration.\
+                          \nThey are related in that greater engine displacement produces more power while less increases effiency")
+if name == 'main':
+        main()
